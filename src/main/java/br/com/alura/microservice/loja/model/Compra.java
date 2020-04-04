cdbf861ea7160749c87145de3b900e11b99fb94f
Ramon Gimenes <ramongimee@hotmail.com>
2020-04-04 13:47:19 -0300

    first commit

---
+++ src/main/java/br/com/alura/microservice/loja/model/Compra.java
@@ -0,0 +1,61 @@
+package br.com.alura.microservice.loja.model;
+
+import java.time.LocalDate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Compra {
+
+	@Id
+	private Long pedidoId;
+	
+	private Integer tempoDePreparo;
+	
+	private String enderecoDestino;
+	
+	private LocalDate dataParaEntrega;
+	
+	private Long voucher;
+
+	public Long getPedidoId() {
+		return pedidoId;
+	}
+
+	public void setPedidoId(Long pedidoId) {
+		this.pedidoId = pedidoId;
+	}
+
+	public Integer getTempoDePreparo() {
+		return tempoDePreparo;
+	}
+
+	public void setTempoDePreparo(Integer tempoDePreparo) {
+		this.tempoDePreparo = tempoDePreparo;
+	}
+
+	public String getEnderecoDestino() {
+		return enderecoDestino;
+	}
+
+	public void setEnderecoDestino(String enderecoDestino) {
+		this.enderecoDestino = enderecoDestino;
+	}
+
+	public LocalDate getDataParaEntrega() {
+		return dataParaEntrega;
+	}
+
+	public void setDataParaEntrega(LocalDate dataParaEntrega) {
+		this.dataParaEntrega = dataParaEntrega;
+	}
+
+	public Long getVoucher() {
+		return voucher;
+	}
+
+	public void setVoucher(Long voucher) {
+		this.voucher = voucher;
+	}
+}
---
+++ src/main/java/br/com/alura/microservice/loja/service/CompraService.java
@@ -0,0 +1,80 @@
+package br.com.alura.microservice.loja.service;
+
+import java.time.LocalDate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
+import br.com.alura.microservice.loja.client.FornecedorClient;
+import br.com.alura.microservice.loja.client.TransportadorClient;
+import br.com.alura.microservice.loja.controller.dto.CompraDTO;
+import br.com.alura.microservice.loja.controller.dto.InfoEntregaDTO;
+import br.com.alura.microservice.loja.controller.dto.InfoFornecedorDTO;
+import br.com.alura.microservice.loja.controller.dto.InfoPedidoDTO;
+import br.com.alura.microservice.loja.controller.dto.VoucherDTO;
+import br.com.alura.microservice.loja.model.Compra;
+import br.com.alura.microservice.loja.repository.CompraRepository;
+
+
+@Service
+public class CompraService {
+	
+	private static final Logger LOG = LoggerFactory.getLogger(CompraService.class);
+	
+	@Autowired
+	private FornecedorClient fornecedorClient;
+	
+	@Autowired
+	private TransportadorClient transportadorClient;
+	
+	@Autowired
+	private CompraRepository compraRepository;
+	
+	@HystrixCommand(threadPoolKey = "getByIdThreadPool")
+	public Compra getById(Long id) {
+		return compraRepository.findById(id).orElse(new Compra());
+	}
+	
+	@HystrixCommand(fallbackMethod = "realizaCompraFallback",
+			threadPoolKey = "realizaCompraThreadPool")
+	public Compra realizaCompra(CompraDTO compra) {
+		
+		final String estado = compra.getEndereco().getEstado();
+		
+		LOG.info("Buscando informações do fornecedor de {}",estado);
+		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(estado);
+		
+		LOG.info("Realizando um pedido {}",info);
+		InfoPedidoDTO pedido = fornecedorClient.realizaPedido(compra.getItens());
+		
+		InfoEntregaDTO entregaDTO = new InfoEntregaDTO();
+		entregaDTO.setPedidoId(pedido.getId());
+		entregaDTO.setDataParaEntrega(LocalDate.now().plusDays(pedido.getTempoDePreparo()));
+		entregaDTO.setEnderecoOrigem(info.getEndereco());
+		entregaDTO.setEnderecoDestino(compra.getEndereco().toString());
+		
+		VoucherDTO voucher = transportadorClient.reservaEntrega(entregaDTO);
+		
+		
+		Compra compraSalva = new Compra();
+		compraSalva.setPedidoId(pedido.getId());
+		compraSalva.setTempoDePreparo(pedido.getTempoDePreparo());
+		compraSalva.setEnderecoDestino(compra.getEndereco().toString());
+		compraSalva.setDataParaEntrega(voucher.getPrevisaoParaEntrega());
+		compraSalva.setVoucher(voucher.getNumero());
+		compraRepository.save(compraSalva);
+		
+		return compraSalva;
+		
+	}
+	
+	public Compra realizaCompraFallback(CompraDTO compraDTO) {
+		Compra compraFallBack = new Compra();
+		compraFallBack.setEnderecoDestino(compraDTO.getEndereco().toString());
+		return compraFallBack;
+	}
+}
